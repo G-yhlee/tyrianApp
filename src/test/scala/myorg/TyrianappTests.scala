df3ddf755cc4f0fdd2ccef3de14ad37a1e2b4f59
G-yhlee <yhleesoft@gmail.com>
2023-01-09 17:43:06 +0900

    init with .gitignore file

---
+++ src/test/scala/myorg/TyrianappTests.scala
@@ -0,0 +1,9 @@
+package myorg
+
+class TyrianappTests extends munit.FunSuite {
+
+  test("dummy test") {
+    assert(1 == 1)
+  }
+
+}
---
+++ tyrianapp.js
@@ -0,0 +1,5 @@
+import {
+  TyrianApp
+} from './target/scala-3.2.0/tyrianapp-fastopt.js';
+
+TyrianApp.launch("myapp");
